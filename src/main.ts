cf04cd10d1b22100977528c19cf1a58b927d2e0f
Hasnain-Makhdoom <86113425+Hasnain-Makhdoom@users.noreply.github.com>
2024-09-03 23:05:54 +0500

    added providers to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { provideHttpClient } from "@angular/common/http";
+import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
+import { provideAuth, getAuth } from "@angular/fire/auth";
+import { firebaseConfig } from "./firebase-config";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideHttpClient()
+  ]
+});
