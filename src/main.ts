cd06d23ef480684d702a9d68da9dde7e5cd415fa
Hasnain-Makhdoom <86113425+Hasnain-Makhdoom@users.noreply.github.com>
2024-09-04 12:39:08 +0500

    added routes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,46 @@ import { provideHttpClient } from "@angular/common/http";
 import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
 import { provideAuth, getAuth } from "@angular/fire/auth";
 import { firebaseConfig } from "./firebase-config";
+import { provideRouter } from "@angular/router";
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from "@angular/fire/auth-guard";
+import { LoginComponent } from "./app/components/login/login.component";
+import { RegisterComponent } from "./app/components/register/register.component";
+import { HomeComponent } from "./app/pages/home/home.component";
+import { MovieDetailsComponent } from "./app/pages/movie-details/movie-details.component";
+import { SearchComponent } from "./app/pages/search/search.component";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter([
+      { path: 'register',
+        component: RegisterComponent,
+        ...canActivate(() => redirectLoggedInTo(['/home']))
+      },
+      { path: 'login',
+        component: LoginComponent,
+        ...canActivate(() => redirectLoggedInTo(['/home']))
+
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+        ...canActivate(() => redirectUnauthorizedTo(['/login']))
+      },
+      {
+        path: 'search',
+        component: SearchComponent,
+        ...canActivate(() => redirectUnauthorizedTo([ '/login' ]))
+      },
+      {
+        path: 'movie/:id',
+        component: MovieDetailsComponent,
+        ...canActivate(() => redirectUnauthorizedTo([ '/login' ]))
+      },
+      { path: '', pathMatch: 'full', redirectTo: '/home' },
+      { path: '**', pathMatch: 'full', redirectTo: '/home' }
+    ])
   ]
 });
